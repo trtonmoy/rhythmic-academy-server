7a20c0c997ad2c363dc3e0d0d096fd137e045e99
trtonmoy <tr.tonmoy0110.trt@gmal.com>
2023-06-11 20:17:38 +0600

    initializing the project

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//
+//
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nlhjk6a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const instrumentsCollection = client
+      .db("Rhythmic")
+      .collection("instruments");
+
+    app.get("/instruments", async (req, res) => {
+      const result = await instrumentsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("rhythmic academy is good");
+});
+
+app.listen(port, () => {
+  console.log(`the port is running on the port no: ${port} `);
+});
